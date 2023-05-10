0ae793f0db12289b232653a0d5cb9a11812dc8c9
UMK <haga5032@naver.com>
2023-05-10 16:18:08 +0900

    sentry 상태 코드 수정

--- miniproject/src/main/java/com/example/miniproject/exception/ApiException.java
+++ miniproject/src/main/java/com/example/miniproject/exception/ApiException.java
@@ -2,15 +2,17 @@ package com.example.miniproject.exception;
 
 import com.example.miniproject.config.SentrySupport;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
 public class ApiException extends RuntimeException {
     private ExceptionEnum error;
-    private SentrySupport sentrySupport;
+    private SentrySupport sentrySupport = new SentrySupport();
 
     public ApiException(ExceptionEnum e) {
         super(e.getMessage());
         this.error = e;
-        sentrySupport.logSimpleMessage(e.getMessage());
+        sentrySupport.logSimpleMessage(error.getMessage());
     }
 }
